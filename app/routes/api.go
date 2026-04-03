c378d56466d9bd2f9f1772248c65504f70ef5bab
Muhammad Abdurrahman <rachman.sd@gmail.com>
2026-04-03 19:50:19 +0700

    refactor: remove debug print statement from ApiRoute

--- app/routes/api.go
+++ app/routes/api.go
@@ -1,13 +1,10 @@
 package routes
 
 import (
-	"fmt"
-
 	"github.com/gofiber/fiber/v3"
 )
 
 func ApiRoute(app *fiber.App) {
-	fmt.Println("we here")
 	app.Get("/", func(c fiber.Ctx) error {
 		return c.SendString("Hi, we up")
 	})
