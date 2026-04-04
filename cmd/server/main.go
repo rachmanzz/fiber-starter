b682ada00f57c0820a73423137030ad43d7f095c
Muhammad Abdurrahman <rachman.sd@gmail.com>
2026-04-04 15:21:41 +0700

    refactor: update entrypoint main.go to use new bootstrap lifecycle

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"github.com/rachmanzz/fiber-starter/bootstrap"
-	"go.uber.org/zap"
 )
 
 func main() {
 	app := bootstrap.NewApplication()
-	app.RegisterProviders()
-	if err := app.Bootstrap(); err != nil {
-		zap.L().Info("error start app")
-	}
+	app.Bootstrap()
+	app.Run()
 }
