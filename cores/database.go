75fb283218b4e4f0b5e97d54a25c09ac79b9bfb2
Muhammad Abdurrahman <rachman.sd@gmail.com>
2026-04-04 15:16:48 +0700

    feat: add cores/database.go for postgres connection management

---
+++ cores/database.go
@@ -0,0 +1,66 @@
+package cores
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/rachmanzz/fiber-starter/app/repository/contract"
+	"go.uber.org/zap"
+)
+
+var (
+	db     *pgxpool.Pool
+	dbOnce sync.Once
+)
+
+func ConnectDB() {
+	dbOnce.Do(func() {
+		dsn := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=%s",
+			Config().Database.User,
+			Config().Database.Password,
+			Config().Database.Host,
+			Config().Database.Port,
+			Config().Database.Name,
+			Config().Database.SSLMode,
+		)
+
+		config, err := pgxpool.ParseConfig(dsn)
+		if err != nil {
+			zap.L().Fatal("Failed to parse database DSN", zap.Error(err))
+		}
+
+		// Konfigurasi Pool
+		config.MaxConns = 10
+		config.MinConns = 2
+		config.MaxConnLifetime = 1 * time.Hour
+		config.MaxConnIdleTime = 30 * time.Minute
+
+		pool, err := pgxpool.NewWithConfig(context.Background(), config)
+		if err != nil {
+			zap.L().Fatal("Failed to connect to database", zap.Error(err))
+		}
+
+		// Verifikasi koneksi dengan timeout 5 detik
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if err := pool.Ping(ctx); err != nil {
+			zap.L().Fatal("Database ping failed", zap.Error(err))
+		}
+
+		zap.L().Info("Database connection established")
+
+		db = pool
+
+		contract.DatabaseContract(pool)
+	})
+}
+func CloseDB() {
+	if db != nil {
+		db.Close()
+		zap.L().Info("Database connection pool closed")
+	}
+}
